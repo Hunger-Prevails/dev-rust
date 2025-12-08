38fb30ccee5089b8292d79ca892677cdbff531cb
Hunger-Prevails <yinglun.liu.it.happens@gmail.com>
2025-12-08 11:50:12 +0100

    file reader

--- helloworld.rs
+++ helloworld.rs
@@ -1,6 +1,28 @@
 use std::f64::consts;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::env;
+
+
+fn read_lines(filename: String) -> io::Result<Vec<String>> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+
+    let mut lines: Vec<String> = Vec::new();
+
+    for line_result in reader.lines() {
+        let line = line_result?;
+        let trimmed = line.trim_end();
+        lines.push(trimmed.to_string());
+    }
+    return Ok(lines);
+}
+
 
 fn main() {
+	let filepath = env::args().nth(1).expect("please supply an argument");
+
     let mut data: Vec<i32> = (0..10).collect();
 
     data.push(10);
@@ -19,8 +41,10 @@ fn main() {
     let abs_difference = x.sin().abs();
 
     assert!(abs_difference < 1e-10);
-}
 
-fn square(x: f32) -> f32 {
-    return x * x;
+    let read_lines_result = read_lines(filepath);
+
+    if let Ok(names) = read_lines_result {
+        println!("{:?}", names);
+    }
 }
